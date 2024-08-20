feade3d632434f67d76aeaf49b218c1a3daeb0e4
aleifj <sonmja96@naver.com>
2024-08-20 12:11:13 +0900

    0820
    총알 벽과 충돌 시 삭제.

---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {//충돌이 시작할 때 발생하는 이벤트
+        if(collision.collider.CompareTag("BULLET"))
+        {//충돌한 게임오브젝트의 태그값 비교
+            Destroy(collision.gameObject);
+        }
+    }
+}
