3d815fc85e95a207be1a15cfcb22c6a8bce73283
aleifj <sonmja96@naver.com>
2024-10-07 12:27:22 +0900

    씬 분리 및 병합.
    1007

--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 
 public class UIManager : MonoBehaviour
@@ -15,20 +16,20 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         //UnityAction을 사용한 이벤트 연결 방식.
-        action = () => OnButtonClick(startButton.name);
+        action = () => OnStartClick();
         startButton.onClick.AddListener(action);
         //무명메서드를 사용한 이벤트 연결 방식, 세미클론 이상한데 들어가네...
         optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name); });
         //람다식을 사용한 이벤트 연결 방식.464p 참고
         shopButton.onClick.AddListener(() => OnButtonClick(shopButton.name));
     }
-
-    void Update()
-    {
-        
-    }
     public void OnButtonClick(string msg)
     {
         Debug.Log($"Click Button : {msg}");
+    }
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("Level_01");
+        SceneManager.LoadScene("Play",LoadSceneMode.Additive);
     }
 }
